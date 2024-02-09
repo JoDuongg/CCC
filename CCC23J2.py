de4ad73fc1b8a5c39c3f83d5c2c6b0b45511d380
JoDuongg <87959856+JoDuongg@users.noreply.github.com>
2024-02-09 12:25:23 -0500

    code

---
+++ CCC23J2.py
@@ -0,0 +1,9 @@
+
+n = int(input("enter number of peppers: "))
+
+pepperName = ["Poblano", "Mirasol", "Serrano", "Cayenne", "Thai", "Habanero"]
+heatUnit = [1500, 6000, 15500, 40000, 75000, 125000]
+
+for i in range(n):
+    name = str(input("Enter names: "))
+    
